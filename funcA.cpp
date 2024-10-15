3077a56ac81a89157336315650cfd935bad1e6c0
DESKTOP-4LAJOIL\Andrey <www.vandrey@gmail.com>
2024-10-15 20:04:00 +0300

    BranchA: finished implementation with comments

--- funcA.cpp
+++ funcA.cpp
@@ -1,5 +1,10 @@
 #include "FuncA.h"
 
-double FuncA::calculate() {
-    return 0;  // Початкова реалізація функції повертає 0
+// Функція обчислює суму перших n елементів нескінченної послідовності
+double FuncA::calculate(int n) {
+    double sum = 0;
+    for (int i = 0; i < n; ++i) {
+        sum += 1.0 / (1 << i);
+    }
+    return sum;
 }
